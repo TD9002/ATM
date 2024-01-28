8ed6bd517d056f9ce0bd52740cf04b0484534b6d
TD9002 settings <srhgithub@mein.gmx>
2024-01-28 17:01:03 +0100

    added basic pdf printer

--- src/main/java/srhe/dittmar/pdfPrinter/PdfPrinter.java
+++ src/main/java/srhe/dittmar/pdfPrinter/PdfPrinter.java
@@ -6,14 +6,10 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import srhe.dittmar.account.Account;
-import srhe.dittmar.account.Transaction;
 
-import javax.swing.table.TableRowSorter;
 import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
-import java.util.List;
 
 public class PdfPrinter {
 
@@ -22,13 +18,17 @@ public class PdfPrinter {
         PDPage my_page = new PDPage();
         document.addPage(my_page);
         PDPageContentStream contentStream = new PDPageContentStream(document, my_page);
+
         contentStream.beginText();
         contentStream.newLineAtOffset(25, 700);
+
         String headline = "Account number %d  Account owner: %s";
         headline = String.format(headline, account.getAccountNumber(), account.getCustomerName());
+
         contentStream.setFont(new PDType1Font(Standard14Fonts.FontName.HELVETICA), 14);
         contentStream.setLeading(14.5f);
         contentStream.showText(headline);
+
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'|'HH:mm:ss");
         for (int i = 0; i < account.getTransactionList().size(); i++) {
 
@@ -38,18 +38,19 @@ public class PdfPrinter {
             ZonedDateTime date = account.getTransactionList().get(i).getTransactionTime();
             String time = dateTimeFormatter.format(date);
             contentStream.newLine();
-            contentStream.showText(i + 1 + ".  " + time + " || " +transaction + String.format("%+,12.2f€",amount));}
-            contentStream.newLine();
-            contentStream.showText("");
-            contentStream.newLine();
-            contentStream.showText("Account balance:" + String.format("%+,44.2f€", account.getAccountValue()));
-            contentStream.endText();
-            contentStream.close();
-            String home = System.getProperty("user.home");
-            document.save(home+"/documents/" + "accountStatement.pdf");
-            System.out.println("PDF created");
-            //Closing the document
-            document.close();
+            contentStream.showText(i + 1 + ".  " + time + " || " +transaction + String.format("%+,12.2f€",amount));
+        }
+        contentStream.newLine();
+        contentStream.showText("");
+        contentStream.newLine();
+        contentStream.showText("Account balance:" + String.format("%+,44.2f€", account.getAccountValue()));
+        contentStream.endText();
+        contentStream.close();
+
+        String home = System.getProperty("user.home");
+        document.save(home+"/documents/" + "accountStatement.pdf");
+        System.out.println("PDF created");
+        document.close();
         }
     }
 
