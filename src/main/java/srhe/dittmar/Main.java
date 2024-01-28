e5d7496affa4d0204e27dc41f0253853db055034
TD9002 settings <srhgithub@mein.gmx>
2024-01-28 16:57:43 +0100

    added basic pdf printer

--- src/main/java/srhe/dittmar/Main.java
+++ src/main/java/srhe/dittmar/Main.java
@@ -1,12 +1,4 @@
 package srhe.dittmar;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDFont;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
-import srhe.dittmar.pdfPrinter.PdfPrinter;
 import srhe.dittmar.ui.startMenu.StartMenu;
 
 import java.io.IOException;
